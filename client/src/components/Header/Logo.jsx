75d79e21d50d25519d3d2dc3892759816379b428
Kunal <carrierhub41@gmail.com>
2026-06-20 12:24:03 +0530

    Fix logo image height in Logo component

--- client/src/components/Header/Logo.jsx
+++ client/src/components/Header/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
 			href='/'
 			className=' flex items-center gap-2  min-w-32 transition-all'>
 			<img src="/images/adroit.png"
-			className="w-auto h-auto md:w-33 md:h-auto object-containa"
+			className="w-auto h-25 md:w-33 md:h-auto object-containa"
 			/>
 		</a>
 	);

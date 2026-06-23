dc6ccd1918234e28e75a69326143919a7b672de0
KunalSolutions <carrierhub41@gmail.com>
2026-06-23 11:43:06 +0530

    update with grid

--- client/src/components/Brand/BrandSection.jsx
+++ client/src/components/Brand/BrandSection.jsx
@@ -160,7 +160,7 @@ export default function BrandSection() {
             Home Audio & Hi-Fi
           </h3>
 
-           <div className="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
+           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-6">
 
             {studioBrands.map((studio, index) => (
                 <div
@@ -185,7 +185,7 @@ export default function BrandSection() {
             Studio & Monitoring
           </h3>
             
-            <div className="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-6">
 
             {homeBrands.map((home, index) => (
                 <div
